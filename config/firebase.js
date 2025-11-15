9b9c0a1bb4818d70dd7bce903fd47d295a6fd8a7
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-15 10:02:06 +0000

    Firebase

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrUTghcXFNAj89_TFuXwtoy_BYv2CyLLA",
+  authDomain: "cchat-7d890.firebaseapp.com",
+  projectId: "cchat-7d890",
+  storageBucket: "cchat-7d890.firebasestorage.app",
+  messagingSenderId: "4592597992",
+  appId: "1:4592597992:web:4a6d301fdb97c97b53c25d",
+  measurementId: "G-NW781MC10T",
+};
+
+const app = initializeApp(firebaseConfig);
