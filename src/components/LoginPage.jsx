6291b282c42e30b40951955bc87af7315019b707
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-15 23:12:27 +0000

    login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,16 +3,21 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default function LoginPage() {
   return (
-    <>
-      <div>Login Page</div>
+    <div className="flex flex-col items-center h-screen justify-center">
+      <div className="w-24 rounded-xl">
+        <img src="./icon.png" alt="Logo" />
+      </div>
+      <div className="text-3xl font-bold text-green-500">cchat</div>
       <button
+        className="btn btn-primary mt-3"
         onClick={() => {
           const provider = new GoogleAuthProvider();
           signInWithPopup(auth, provider);
         }}
       >
         Login with Google
+        <i class="fa-brands fa-google fa-beat"></i>
       </button>
-    </>
+    </div>
   );
 }
