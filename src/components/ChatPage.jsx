249e25a7422b037ff01c11028401a1259c4ffc8a
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-16 20:56:16 +0000

    layout

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -10,14 +10,14 @@ export default function ChatPage() {
 
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex gap-2 p-4">
+      <div className="flex gap-2 p-3">
         <button className="cursor-pointer" onClick={() => navigate("/")}>
           <i className="fa-solid fa-arrow-left"></i>
         </button>
         <h1>Nome do chat</h1>
       </div>
 
-      <div className="flex-1 overflow-y-auto p-4">
+      <div className="flex-1 overflow-y-auto p-3">
         {messages.map((message) => (
           <MessageBubble
             key={message.id}
@@ -30,7 +30,7 @@ export default function ChatPage() {
         ))}
       </div>
 
-      <MessageInput className="p-4" />
+      <MessageInput className="p-3" />
     </div>
   );
 }
