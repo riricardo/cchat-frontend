e8f373b7d8c5b72bed56002331c5a1d1d634d6e3
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-16 10:13:05 +0000

    Initial homepage styling

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,20 +2,24 @@ import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
 import MessageBubble from "./MessageBubble";
 
+function TabContent({ children }) {
+  return (
+    <div className="tab-content bg-base-100 border-base-300 p-6 h-screen">
+      {children}
+    </div>
+  );
+}
+
 export default function HomePage() {
   return (
     <>
-      <div>Home Page</div>
-      <button onClick={() => signOut(auth)}>Sign Out</button>
-
-      {/* name of each tab group should be unique */}
       <div className="tabs tabs-lift">
         <label className="tab">
-          <input type="radio" name="my_tabs_4" />
+          <input type="radio" name="my_tabs" defaultChecked />
           <i class="fa-solid fa-comments mr-2"></i>
           Chat
         </label>
-        <div className="tab-content bg-base-100 border-base-300 p-6">
+        <TabContent>
           <MessageBubble
             profileImageUrl="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
             userName="Obi-Wan Kenobi"
@@ -30,10 +34,10 @@ export default function HomePage() {
             text="I hate you!"
             isLoggedUser={true}
           />
-        </div>
+        </TabContent>
 
         <label className="tab">
-          <input type="radio" name="my_tabs_4" defaultChecked />
+          <input type="radio" name="my_tabs" />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -50,31 +54,18 @@ export default function HomePage() {
           </svg>
           Laugh
         </label>
-        <div className="tab-content bg-base-100 border-base-300 p-6">
-          Tab content 2
-        </div>
+        <TabContent>Tab content 2</TabContent>
 
         <label className="tab">
-          <input type="radio" name="my_tabs_4" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-4 me-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-            />
-          </svg>
-          Love
+          <input type="radio" name="my_tabs" />
+          <i class="fa-solid fa-gear mr-2"></i>
+          Settings
         </label>
-        <div className="tab-content bg-base-100 border-base-300 p-6">
-          Tab content 3
-        </div>
+        <TabContent>
+          <button className="btn btn-warning" onClick={() => signOut(auth)}>
+            Sign Out
+          </button>
+        </TabContent>
       </div>
     </>
   );
