2e30e141b8ba27da206e1da9ffc70ffcc79e4c35
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-16 23:41:36 +0000

    List of users

--- src/components/pages/AddUserPage.jsx
+++ src/components/pages/AddUserPage.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import InputSearch from "../core/InputSearch";
 import NavigationHeader from "../core/NavigationHeader";
+import usersTest from "../tests/users-test";
 
 export default function ChatPage() {
   const navigate = useNavigate();
@@ -14,6 +15,20 @@ export default function ChatPage() {
       />
 
       <InputSearch className="p-3" />
+
+      <ul className="list bg-base-100 rounded-box">
+        {usersTest.map((user) => {
+          return (
+            <li key={user.id} className="list-row flex">
+              <div className="flex gap-3 flex-1 items-center">
+                <img className="size-10 rounded-box" src={user.profileUrl} />
+                <div>{user.name}</div>
+              </div>
+              <button className="btn btn-accent justify-end">Start chat</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/tabs/ChatsTab.jsx
+++ src/components/tabs/ChatsTab.jsx
@@ -1,5 +1,4 @@
 import chats from "../tests/chats-test";
-import ChatItem from "../core/ChatItem";
 import { useNavigate } from "react-router-dom";
 
 export function ChatTabHeader() {
@@ -18,15 +17,30 @@ export default function ChatTab() {
     navigate(`/chat/${chatId}`);
   }
 
+  function getChatImage(chat) {
+    return chat.isUserChat
+      ? chat.users[0].profileImageUrl
+      : chat.group.groupImageUrl;
+  }
+
+  function getChatTitle(chat) {
+    return chat.isUserChat ? chat.users[0].name : chat.group.groupName;
+  }
+
   return (
     <ul className="list bg-base-100 rounded-box">
       {chats.map((chat) => {
         return (
-          <ChatItem
+          <li
             key={chat.id}
-            chatDetails={chat}
+            className="list-row flex items-center cursor-pointer"
             onClick={() => handleChatClick(chat.id)}
-          />
+          >
+            <div>
+              <img className="size-10 rounded-box" src={getChatImage(chat)} />
+            </div>
+            <div>{getChatTitle(chat)}</div>
+          </li>
         );
       })}
     </ul>
