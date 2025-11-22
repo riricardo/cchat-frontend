b56f7de41d971a87e4f5288d1564f60e4bafa9cb
riricardo <ricardo.augusto.cardona@gmail.com>
2025-11-22 10:09:38 +0000

    fixing environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3jeYnDRPVB6ZAsPKMytCWsge4lhGytqM",
-  authDomain: "cchat-app-105f1.firebaseapp.com",
-  projectId: "cchat-app-105f1",
-  storageBucket: "cchat-app-105f1.firebasestorage.app",
-  messagingSenderId: "861298846878",
-  appId: "1:861298846878:web:296da1f8f39dcd5a64df8f",
-  measurementId: "G-LVRFVJDD4Q",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
